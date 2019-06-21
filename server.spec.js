3cff489bb289532eeba15a0e0ce259273de33d9f
cashcoder88 <cashglobe@gmail.com>
2019-06-21 10:01:29 -0600

    Tests for GET are written, now to add endpoint

--- server.spec.js
+++ server.spec.js
@@ -7,9 +7,25 @@ describe('TEST', () => {
         return supertest(server).get('/').expect(200)
      });
 });
-// describe('GET', () => {
+describe('GET', () => {
     
-// });
+    it('responds with 200 OK', () => {
+        return supertest(server).get('/games').expect(200)
+     });
+     it('responds with Some Json Content', () => {
+        return supertest(server).get('/games').expect('Content-Type', /json/i)
+     });
+     it('responds with empty array when no data is in games', () => {
+         return supertest(server).get('/games')
+         .then(res => {
+             if(res.body.length < 1) {
+               expect(res.body).toEqual({})
+             } else {
+                expect(200)
+             }
+         })
+     });
+});
 
 // describe('POST', () => {
     
